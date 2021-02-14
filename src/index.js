b6e7a5b8e3faa09656727e74bce75f7f36bafb86
d_liyanage <admliyanage@gmail.com>
2021-02-14 20:15:43 +0530

    image card completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/main.css';
 import App from './App';
+require('dotenv').config();
+
 
 ReactDOM.render(
   <React.StrictMode>
